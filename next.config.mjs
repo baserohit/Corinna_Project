98c4211ecd4a0499fc49c20f9f942d82f243789d
Rohit Singh <baserohit8@gmail.com>
2025-06-11 09:34:12 +0530

    Publish to Github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ucarecdn.com",
+      },
+      {
+        protocol: "http",
+        hostname: "34.171.208.29",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
